d4d5ee5fe72800014fca17e268151ef37fc09461
fatuma65 <namalefatuma28@gmail.com>
2024-04-03 16:38:43 +0300

    deleting package-lock file

--- utilis/helpers.js
+++ utilis/helpers.js
@@ -4,8 +4,8 @@ const hashPassword = async (raw, hash) => {
     return await bcrypt.compare(raw, hash)
 }
 
-const hashed =  (password) => {
-    const salt = bcrypt.genSaltSync()
+const hashed = (password) => {
+    const salt =  bcrypt.genSaltSync()
     return bcrypt.hashSync(password, salt)
 }
 module.exports = {
